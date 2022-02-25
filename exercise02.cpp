cbb50f073195cf18e92dc84a52192f650ef6a9f6
it21344756 <99312095+it21344756@users.noreply.github.com>
2022-02-25 15:33:12 +0000

    change some thing

--- exercise02.cpp
+++ exercise02.cpp
@@ -2,12 +2,13 @@
 using namespace std;
 
 int volume(int height, int width, int length);
-
+//function prototype
 int main() {
+  //define veriable
     int box1Height, box1Width, box1Length;
     int box2Height, box2Width, box2Length;
     int totalVolume, totalSurface;
-    
+  // user input
     cout << "Enter Box 1 Height : ";
     cin >> box1Height;
     cout << "Enter Box 1 Width : ";
@@ -21,13 +22,20 @@ int main() {
     cin >> box2Width;
     cout << "Enter Box 2 Length : ";
     cin >> box2Length;
-    
+    //calculation
     totalVolume = volume(box1Height, box1Width, box1Length)
              + volume(box2Height, box2Width, box2Length);
-             
+    //display         
     cout << "Volume of Box is " << totalVolume << endl;
     
     return 0;
 }
 
 // Implement the Volume() function here
+int volume(int height,int width,int length)
+{
+  int vol;
+  vol =height*width*length;
+  return vol;
+}
+
